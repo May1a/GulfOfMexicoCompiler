8eb8dedfddaa2f59d1c15fcef29d9eb043b3e905
May1a <mgrass2607@icloud.com>
2025-10-24 17:45:48 +0200

    added errors.zig file to have a place to put the errors

---
+++ src/errors.zig
@@ -0,0 +1,1 @@
+pub const invalidVarDecl = "var/const declaration needs to be followed by another var/const declaration \n example: \n var var myVariable => Valid \n const myVariable => NOT Valid! \n";
