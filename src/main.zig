b8492bbf11f3b3034ebb203d235978d24a7620e1
May1a <mgrass2607@icloud.com>
2025-10-28 21:49:22 +0100

    Fixed very stupid bug

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,7 @@ const zGoM = @import("zGoM");
 pub fn main() !void {
     const f = try std.fs.cwd().openFile("samples/test.gom", .{});
     defer f.close();
-    var buffer: [1024]u8 = undefined;
+    var buffer: [2048]u8 = undefined;
     const n = try f.read(&buffer);
     std.debug.print("Contents: {s}\n", .{buffer[0..n]});
     try zGoM.interpret(buffer[0..n], true);
