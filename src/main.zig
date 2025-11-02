ae41aeb871fbb4314eaf27f3266a1c427df05c8d
May1a <mgrass2607@icloud.com>
2025-11-02 14:27:59 +0100

    Added a new test for line counting (Incomplete)

--- src/main.zig
+++ src/main.zig
@@ -10,21 +10,11 @@ pub fn main() !void {
     try zGoM.interpret(buffer[0..n]);
 }
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+test "Line Counting" {
+    const f = try std.fs.cwd().openFile("samples/simple_line_test.gom", .{});
+    defer f.close();
+    var buffer: [2048]u8 = undefined;
+    const n = try f.read(&buffer);
+    std.debug.print("Contents: {s}\n", .{buffer[0..n]});
+    try zGoM.interpret(buffer[0..n]);
 }
