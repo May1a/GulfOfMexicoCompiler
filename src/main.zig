3e289cff711361c0acb8fe3694113a1b7f334a2d
May1a <mgrass2607@icloud.com>
2025-11-04 18:29:44 +0100

    pass in an actual arena

--- src/main.zig
+++ src/main.zig
@@ -5,12 +5,15 @@ const Parser = @import("parser.zig");
 pub fn main() !void {
     var allocator = std.heap.c_allocator;
     _ = &allocator;
+    var arenaAlloc = std.heap.ArenaAllocator.init(allocator);
+    const arena = arenaAlloc.allocator();
+
     const f = try std.fs.cwd().openFile("samples/test.gom", .{});
     defer f.close();
     var buffer: [2048]u8 = undefined;
     const n = try f.read(&buffer);
     std.debug.print("Contents: {s}\n", .{buffer[0..n]});
-    var parser = Parser.init(allocator);
+    var parser = Parser.init(arena);
     const parsed = try parser.parse(buffer[0..n]);
     std.debug.print("All Tokens:\n", .{});
     var identIdx: u16 = 0;
