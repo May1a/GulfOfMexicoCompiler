1ef4fec36d0e08f0c7a1097bf64d36dfc841d2c4
May1a <mgrass2607@icloud.com>
2025-11-23 01:05:52 +0100

    updated main

--- src/main.zig
+++ src/main.zig
@@ -1,25 +1,16 @@
 const std = @import("std");
-const zGoM = @import("root.zig");
+
+const AST = @import("ast.zig");
 const Parser = @import("parser.zig");
+const zGoM = @import("root.zig");
 
-pub fn main() !void {
-    var allocator = std.heap.c_allocator;
-    _ = &allocator;
-    var arenaAlloc = std.heap.ArenaAllocator.init(allocator);
-    const arena = arenaAlloc.allocator();
-
-    const f = try std.fs.cwd().openFile("samples/test.gom", .{});
-    defer f.close();
-    var buffer: [2048]u8 = undefined;
-    const n = try f.read(&buffer);
-    std.debug.print("Contents: {s}\n", .{buffer[0..n]});
-    var parser = Parser.init(arena);
-    const parsed = try parser.parse(buffer[0..n]);
-    std.debug.print("All Tokens:\n", .{});
+fn printTokens(parsed: []Parser.Token) void {
     var identIdx: u16 = 0;
     var numIdx: u16 = 0;
     var strIdx: u16 = 0;
-    for (parsed.Tokens) |t| {
+    std.debug.print("All Tokens:\n", .{});
+
+    for (parsed) |t| {
         if (t.type == .Ident) {
             std.debug.print("{any} - {s}\n", .{ t, parsed.idents[identIdx] });
             identIdx += 1;
@@ -31,6 +22,29 @@ pub fn main() !void {
             numIdx += 1;
         } else std.debug.print("{any}\n", .{t});
     }
+}
+
+pub fn main() !void {
+    try std.fs.File.stdout().writeAll("Hello, World!\n");
+    var allocator = std.heap.page_allocator;
+    _ = &allocator;
+    var arenaAlloc = std.heap.ArenaAllocator.init(allocator);
+    const arena = arenaAlloc.allocator();
+
+    var args = try std.process.ArgIterator.initWithAllocator(arena);
+    _ = args.skip();
+    const fileN = args.next() orelse "samples/test.gom";
+    const f = try std.fs.cwd().openFile(fileN, .{});
+    defer f.close();
+    var buffer: [2048]u8 = undefined;
+    const n = try f.read(&buffer);
+    std.debug.print("Contents: {s}\n", .{buffer[0..n]});
+    var parser = Parser.init(arena);
+    const parsed = try parser.parse(buffer[0..n]);
+
+    var ast = AST.init(parsed, arena);
+    const parsedRoot = try ast.parseRoot();
+    std.debug.print("{any}\n", .{parsedRoot});
 }
 
 test "Line Counting" {
